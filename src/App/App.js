f215963e769ff3eda1d25716dadfcb8c2111b226
Stasy <stasya.tyapkina@gmail.com>
2022-07-05 10:06:07 +0200

    build

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,10 @@
 import { Main } from "../page/Main";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-
-const theme = createTheme();
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <div>
-        <Main />
-      </div>
-    </ThemeProvider>
+    <div>
+      <Main />
+    </div>
   );
 }
 
